fa32d5a153e49891d72ad6a670dce25ddb4f2942
Logan McDonald <loganmcdona11@gmail.com>
2015-10-19 15:22:10 -0700

    Added some primaries info

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,7 +1,29 @@
 module FarMar
 	class Market
-		def initialize
-			
+
+		attr_accessor :identifier, :name, :address, :city, :county, :state, :zip
+
+		def initialize(market_hash)
+			@identifier = market_hash[:identifier]
+			@name = 			market_hash[:name]
+			@address = 		market_hash[:address]
+			@city = 			market_hash[:city]
+			@county = 		market_hash[:county]
+			@state = 			market_hash[:state]
+			@zip = 				market_hash[:zip]
+		end
+		def self.all
+			market_csv = CSV.read("./support/market.csv")
+
+			market_array = []
+
+			owner_csv.each do |row|
+				market_hash = {:identifier => row[0], :name => row[1], 
+											 :address => row[2], :street => row[3], 
+											 :city => row[4], :state => row[5]}
+				owners_array.push(Market.new(market_hash))
+			end
+			return owners_array
 
 			#returns a collection of Market instances, 
 			#representing all of the markets described 
